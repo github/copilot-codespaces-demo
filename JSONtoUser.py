ba27ea45f5c5302b8ac379034c64ea9c03e42441
Jacob Kirsch <55998770+ThisIsKirsch@users.noreply.github.com>
2023-05-16 20:08:21 +0000

    Update JSONtoUser

---
+++ JSONtoUser.py
@@ -0,0 +1,17 @@
+class User:
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+
+user = {
+    "name": "John Doe",
+    "email": "johndoe@gmail.com",
+    "password": "password123"
+}
+
+#TODO: write a function that takes in a json and returns a User class
+def createUser(json):
+
+#TODO: create a function that takes in a User class and returns a json
+def jsonifyUser(user):
