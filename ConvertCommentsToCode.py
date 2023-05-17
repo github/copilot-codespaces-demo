21a15fea776c6706ffcc001fffe38e96de5291b3
Jacob Kirsch <55998770+ThisIsKirsch@users.noreply.github.com>
2023-05-17 01:15:35 +0000

    updated tours, removed unit test tour, removed final version of tours

---
+++ ConvertCommentsToCode.py
@@ -0,0 +1,4 @@
+# write a function that counts from 1 to 10 and prints the numbers
+def count_to_ten():
+
+count_to_ten()
--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -3,37 +3,3 @@
 import random
 # define main function that handles all the logic
 def main():
-    # create a list of choices
-    choices = ['rock', 'paper', 'scissors']
-    # create a variable to control the loop
-    again = 'y'
-    # create a while loop to control the game
-    while again == 'y' or again == 'Y':
-        # get the user's choice
-        user_choice = input('Enter rock, paper, or scissors: ')
-        # get the computer's choice
-        computer_choice = random.choice(choices)
-        # determine the winner
-        winner = determine_winner(user_choice, computer_choice)
-        # display the results
-        print('You chose', user_choice)
-        print('The computer chose', computer_choice)
-        print('The winner is', winner)
-        # do this again?
-        again = input('Do you want to play again? (y = yes): ')
-# define the determine_winner function
-def determine_winner(user, computer):
-    # rock beats scissors
-    if user == 'rock' and computer == 'scissors':
-        return 'user'
-    # paper beats rock
-    elif user == 'paper' and computer == 'rock':
-        return 'user'
-    # scissors beats paper
-    elif user == 'scissors' and computer == 'paper':
-        return 'user'
-    # computer wins
-    else:
-        return 'computer'
-# call the main function
-main()
